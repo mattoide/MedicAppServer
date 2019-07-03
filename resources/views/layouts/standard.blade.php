5a1a6ed51b2bd95de0b609f6b8778621f95e2943
Matto <mattiabellotti.dev@gmail.com>
2019-07-03 22:46:38 +0200

    a
    a

--- resources/views/layouts/standard.blade.php
+++ resources/views/layouts/standard.blade.php
@@ -34,28 +34,75 @@
 </head>
 
 <body>
-
-  <div class="container-fluid" style=" max-height: 20em;  overflow-y: scroll"></div>
-  <div class="d-flex p-2 sticky-top" style="background-color: #9b8e6e; justify-content: center">
+{{-- 
+  <div class="d-flex p-2 sticky-top" style="background-color: #9b8e6e; justify-content: center; z-index: 1031;">
 
     <a id="novus"  class="nav-link" style="color: white" target="_blank" href="https://novusstudios.it/">Chirurgia Estetica del Piede - by Novus Studios</a>
-    {{-- <nav class="navbar fixed-top nav mx-auto" style="background-color: #9b8e6e; align-content: center; z-index: 1;">
+
+
+  </div> --}}
+
+      <nav class="navbar fixed-top nav mx-auto" style="background-color: #9b8e6e; align-content: center; z-index: 1032;">
 
         <ul class="nav navbar-nav mx-auto">
-            <li class="nav-item"><a id="novus" style="color: white" class="nav-link" target="_blank" href="https://novusstudios.it/">Chirurgia Estetica del Piede - by Novus Studios</a></li>
+            <li class="nav-item"><a id="novus"  class="nav-link" style="color: white" target="_blank" href="https://novusstudios.it/">Chirurgia Estetica del Piede - by Novus Studios</a></li>
         </ul>
 
-    </nav> --}}
+    </nav>
 
-  </div>
-  <div class="row" style="height: 100%; margin-right: 0%; margin-left: 0%;">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-left">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-    {{-- <div class="d-flex position-sticky"> --}}
-<div class="col-1" style="padding: 0%;">
-    <div class="col-1 position-fixed" style="background-color: #333333; height: 100%;">
-      <nav id="sidebar">
-     
-        <ul class="list-unstyled components ">
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    
+        <ul class="navbar-nav">
+
+            <li class="nav-itemno navtm">
+                <a href="#"> 
+               <p></p></a>
+             </li >      <li class="nav-itemno navtm">
+                <a href="#"> 
+               <p></p></a>
+             </li >
+          <li class="nav-item navtm">
+            <a href="/pazienti"> <i class="fas fa-bed fa-lg circle"></i>
+           <p id='pazientii' class="nav-link navtext" href="/pazienti">Pazienti</p></a>
+         </li>
+
+         <li class="nav-item navtm">
+             <a href="/diagnosi"> <i class="far fa-address-card fa-lg circle"></i>
+           <p id='diagnosii' class="nav-link navtext" href="/diagnosi">Diagnosi</p></a>
+         </li>
+
+         <li class="nav-item navtm">
+             <a href="/allergie"> <i class="fas fa-exclamation-triangle fa-lg circle"></i>
+           <p id='allergiee' class="nav-link navtext" href="/allergie">Allergie</p></a>
+         </li>
+
+         <li class="nav-item navtm">
+             <a href="/medicinali"> <i class="fas fa-pills fa-lg circle"></i>
+           <p id='medicinalii' class="nav-link navtext" href="/medicinali">Medicinali</p></a>
+         </li>
+
+         <li class="nav-item navtm">
+             <a href="/reminders">  <i class="far fa-bell fa-lg circle"></i>
+           <p id='reminderss' class="nav-link navtext" href="/reminders">Reminder</p></a>
+         </li>
+
+         <li class="nav-item navtm">
+             <a href="/protocolli"> <i class="fas fa-list-ul fa-lg circle"></i>
+           <p id='protocollii' class="nav-link navtext" href="/protocolli">Protocolli</p></a>
+         </li>
+
+        </ul>
+    </div>
+</nav>
+
+  {{-- <div class="navbar navbar-inverse navbar-fixed-left">
+    <a class="navbar-brand" href="#">Brand</a>
+    <ul class="nav navbar-nav">
       
           <li class="nav-item navtm">
              <a href="/pazienti"> <i class="fas fa-bed fa-lg circle"></i>
@@ -88,24 +135,20 @@
           </li>
 
         </ul>
-      </nav>
 
-    </div>
-  </div>
+    </div> --}}
   {{-- </div> --}}
 
-    <div class="col" style="padding: 0%; margin-top: 4%;">
 {{-- 
         <div class="container-fluid" style="background-color: white;">
         <h3 id="titolo"></h3>
         </div> --}}
-
+<div class="container miocont">
       <nav class="navbar navbar-expand-lg navbar-light bg-light" >
         <h3 id="titolo"></h3>
       </nav>
 <div class="container-fluid" style=" overflow-y: scroll">
       @yield('content')
-    </div>
     </div>
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
